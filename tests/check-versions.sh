885e2029b28d3524eea12fde704e76c300a21654
Patrick Connolly <patrick@myplanetdigital.com>
2012-03-15 15:57:50 -0400

    More version checks for sanity

--- tests/check-versions.sh
+++ tests/check-versions.sh
@@ -1,16 +1,30 @@
 # Format is as follows:
 # COMPONENT remaining lines are command for retrieving version
 COMMAND_MATRIX=$(cat <<EOF
-Varnish varnishstat -V 2>&1 | grep -i ^varnish
-PHP php -v | grep ^PHP | cut -d' ' -f2
-PEAR pear -V 2>&1 | grep ^PEAR | cut -d' ' -f3
-Memcached memcached -h 2>&1 | grep -i ^memcached | cut -d' ' -f2
-Memcached-PECL pecl list | grep -E memcached[[:space:]]+
-Memcached-APT dpkg-query -W | grep -E php5-memcached[[:space:]]+ | cut -f2
+Varnish varnishstat -V 2>&1 | grep -i ^varnish | cut -d' ' -f2 | cut -d- -f2
+Git git --version | awk '{print \$NF}'
+drush drush --version | awk '{print \$NF}'
+drush_make drush make --version -n 2>/dev/null | grep -E ^[[:digit:]]
+PHP php -v | grep ^PHP | awk '{print \$2}'
+PEAR pear -V 2>&1 | grep ^PEAR | awk '{print \$3}'
+APC-PHP-cli       php --ri apc       | grep -i ^version | awk '{print \$3}'
+Memcache-PHP-cli  php --ri memcache  | grep -i ^version | awk '{print \$3}'
+Memcached-PHP-cli php --ri memcached | grep -i ^version | awk '{print \$3}'
+Xdebug-PHP-cli    php --ri xdebug    | grep -i ^version | awk '{print \$3}'
+Memcached memcached -h 2>&1 | grep -i ^memcached | awk '{print \$2}'
+APC-PECL       pecl list | grep APC                      | awk '{print \$2}'
+Memcache-PECL  pecl list | grep memcache                 | awk '{print \$2}'
+Memcached-PECL pecl list | grep -E memcached[[:space:]]+ | awk '{print \$2}'
+Xdebug-PECL    pecl list | grep xdebug                   | awk '{print \$2}'
+APC-APT        dpkg-query -Wf='\${Version}' php*-apc       2>/dev/null
+Memcache-APT   dpkg-query -Wf='\${Version}' php*-memcache  2>/dev/null
+Memcached-APT  dpkg-query -Wf='\${Version}' php*-memcached 2>/dev/null
+Xdebug-APT     dpkg-query -Wf='\${Version}' php*-xdebug    2>/dev/null
 EOF)
 
 # All the servers to test
 servers=(ariadne spartan@ded-1039.prod.hosting.acquia.com)
+#servers=(spartan@ded-1039.prod.hosting.acquia.com)
 
 # Step through COMMAND_MATRIX line by line
 IFS=$'\n'$'\r'
