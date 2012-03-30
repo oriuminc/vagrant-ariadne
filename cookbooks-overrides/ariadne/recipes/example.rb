ec7ae7b88cca67b58ff68ea5ca7e7edbe7f22267
Patrick Connolly <patrick@myplanetdigital.com>
2012-03-30 03:21:34 -0400

    Fixed example.rb typo

--- cookbooks-overrides/ariadne/recipes/example.rb
+++ cookbooks-overrides/ariadne/recipes/example.rb
@@ -53,7 +53,7 @@ bash "Running drush_make..." do
   cwd "/mnt/www/html"
   code <<-EOH
   drush make /vagrant/data/condel/local.condel.build /mnt/www/html/#{project} --verbose --yes
-  (cd #{project} && drush site-install condel --db-url=mysqli://root:#{node['mysql']['server_root_password']}@localhost/#{project}) --yes
+  (cd #{project} && drush site-install condel --db-url=mysqli://root:#{node['mysql']['server_root_password']}@localhost/#{project} --yes)
   EOH
   notifies :restart, "service[varnish]"
   not_if "test -e /mnt/www/html/#{project}"
