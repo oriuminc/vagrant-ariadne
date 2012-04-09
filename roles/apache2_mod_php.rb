c0d3ee57c510e0df225ab49269d90cb78cdbc869
Patrick Connolly <patrick@myplanetdigital.com>
2012-04-09 13:51:33 -0400

    apache2 php.ini symlinked AFTER apache2 recipe.

--- roles/apache2_mod_php.rb
+++ roles/apache2_mod_php.rb
@@ -2,8 +2,8 @@ name "apache2_mod_php"
 description "Configure php5.3 and apache2 with mod_php."
 run_list(
   "recipe[php]",
-  "recipe[php::common_php_ini]",
   "recipe[apache2]",
+  "recipe[php::common_php_ini]",
   "recipe[apache2::mod_expires]",
   "recipe[apache2::mod_php5]",
   "recipe[apache2::mod_rewrite]"
