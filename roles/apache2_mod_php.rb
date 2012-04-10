6af78ef2f4fc9f5a30cfcd922a2a9635869afa65
Patrick Connolly <patrick@myplanetdigital.com>
2012-04-10 14:39:57 -0400

    Fixed again ariadne::common_php_ini being in wrong run_list order.

--- roles/apache2_mod_php.rb
+++ roles/apache2_mod_php.rb
@@ -3,10 +3,10 @@ description "Configure php5.3 and apache2 with mod_php."
 run_list(
   "recipe[php]",
   "recipe[apache2]",
-  "recipe[php::common_php_ini]",
   "recipe[apache2::mod_expires]",
+  "recipe[apache2::mod_rewrite]",
   "recipe[apache2::mod_php5]",
-  "recipe[apache2::mod_rewrite]"
+  "recipe[php::common_php_ini]"
 )
 default_attributes(
   :php => {
