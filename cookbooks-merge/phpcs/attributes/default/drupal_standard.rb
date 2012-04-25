3e279f0546032567ac9dc995e881ad75f846346f
Patrick Connolly <patrick@myplanetdigital.com>
2012-04-25 17:15:32 -0400

    SS-43: Added drupalcs recipe to phpcs

---
+++ cookbooks-merge/phpcs/attributes/default/drupal_standard.rb
@@ -0,0 +1,1 @@
+default['phpcs']['drupalcs_git_ref'] = "7.x-1.0-alpha1"
--- cookbooks-merge/phpcs/recipes/drupal_standard.rb
+++ cookbooks-merge/phpcs/recipes/drupal_standard.rb
@@ -1,19 +1,22 @@
 include_recipe "phpcs"
 
-# Only download if changed
-# Adapted from http://wiki.opscode.com/display/chef/Resources#Resources-RemoteFile
-f = remote_file "drupalcs" do
-  path "#{Chef::Config[:file_cache_path]}/drupalcs-7.x-1.0-alpha1.tar.gz"
-  source "http://ftp.drupal.org/files/projects/drupalcs-7.x-1.0-alpha1.tar.gz"
-  action :nothing
+git "#{Chef::Config[:file_cache_path]}/drupalcs" do
+  repository "http://git.drupal.org/project/drupalcs.git"
+  revision "7.x-1.x"
+  reference node['phpcs']['drupalcs_git_ref']
+  action :sync
 end
 
-http_request "HEAD #{f.source}" do
-  message ""
-  url f.source
-  action :head
-  if File.exists?(f.path)
-    headers "If-Modified-Since" => File.mtime(f.path).httpdate
-  end
-  notifies :create, "remote_file[drupalcs]", :immediately
+bash "copy-drupal-standard" do
+  user "root"
+  code <<-EOH
+    cp -Rf #{Chef::Config[:file_cache_path]}/drupalcs/Drupal $(pear config-get php_dir)/PHP/CodeSniffer/Standards/
+  EOH
+end
+
+bash "create-drupalcs-alias" do
+  user "vagrant"
+  code <<-EOH
+    alias drupalcs='phpcs --standard=Drupal --extensions=php,module,inc,install,test,profile,theme'
+  EOH
 end
