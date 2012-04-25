f92ef9abb91cef648d496db2215348674356970e
Patrick Connolly <patrick@myplanetdigital.com>
2012-04-25 04:29:22 -0400

    SS-20: Added stub for checking local software versions

---
+++ tests/check-local-versions.sh
@@ -0,0 +1,11 @@
+#!/usr/bin
+# @TODO: Check all versions and output a table
+#   to compare recommended to actual.
+sw_vers -productVersion
+xcodebuild -version
+gcc --version
+rvm --version
+ruby --version
+bundle --version
+vagrant --version
+VBoxManage --version
