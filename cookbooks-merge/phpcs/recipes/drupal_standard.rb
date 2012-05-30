a3264d11877bd04f3e99588597f06b3506fb31fb
Patrick Connolly <patrick@myplanetdigital.com>
2012-05-30 11:43:09 -0400

    Added drupalcs alias to phpcs recipe.

--- cookbooks-merge/phpcs/recipes/drupal_standard.rb
+++ cookbooks-merge/phpcs/recipes/drupal_standard.rb
@@ -12,4 +12,12 @@ bash "copy-drupal-standard" do
   code <<-EOH
     cp -Rf #{Chef::Config[:file_cache_path]}/drupalcs/Drupal $(pear config-get php_dir)/PHP/CodeSniffer/Standards/
   EOH
+  not_if "test -d $(pear config-get php_dir)/PHP/CodeSniffer/Standards/Drupal"
+end
+
+file "/home/vagrant/.bash_profile" do
+  owner "vagrant"
+  group "vagrant"
+  content "alias drupalcs='phpcs --standard=Drupal --extensions=php,module,inc,install,test,profile,theme'"
+  action :create_if_missing
 end
